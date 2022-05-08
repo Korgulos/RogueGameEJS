3783836dea09e1a838044f57fd708a3dc13feefb
Your NameKorgulos <you@example.comkaticod100@gmail.com>
2022-05-08 16:54:49 +0200

    more update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RogueGameEJS
 RPG web with ejs
+
+INSTRACTIONS 
+
+# For insatlling dependencies
+npm install
+
+# For starting test serer 
+
+npm start
+
+# Data base MySQL 
+
+data/databace.js => for setup
--- routes/rogue_routes.js
+++ routes/rogue_routes.js
@@ -1,17 +1,33 @@
-const express = require("express");
-const db = require("../data/database")
 
+const express = require("express");
+const db = require("../data/database");
+const Rogue = require("../js_meta/rogue");
 const router = express.Router();
 
-router.get("/",(req,res)=>{
+let roguecaracter = NaN;
+console.log(roguecaracter);
+
+let ap = 0;
+let hp = 0;
+let sd = 0;
+
+router.get("/", (req, res) => {
     res.redirect("main");
 });
 
-router.get("/main",async (req,res)=>{
-    //const [posts] = await db.query("SELECT * FROM posts INNER JOIN authors ON posts.author_id = authors.id_authors");
-    res.render("main");
+router.get("/main", async (req, res) => {
+    const query = `SELECT strength,constitution,dexterity,weaponsMaster,melee,marksman,athletics,defense FROM rogue.character where id_character = 2`;
+    const [data] = await db.query(query);
+    const rg = data[0];
+    roguecaracter = new Rogue(rg.strength, rg.constitution, rg.dexterity, rg.weaponsMaster, rg.melee, rg.marksman, rg.athletics, rg.defense);
+    console.log(roguecaracter.meleeFunction());
+    res.render("main",{roguecaracter:roguecaracter});
 });
 
-
+router.get("/melee", async (req, res) => {
+    
+    //console.log(roguecaracter.meleeFunction());
+    res.render("main");
+});
 
 module.exports = router;
